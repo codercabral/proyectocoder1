8277f92d27eaf30b3949cba4ed4cb2a399d33e53
Ema Cabral <micorreo2@gmail.com>
2024-06-27 20:48:45 -0300

    estoy en la rama

--- ejemplo1.py
+++ ejemplo1.py
@@ -14,3 +14,7 @@ def multiplicar(n1,n2):
     print("el resultado de la multiplicacion entre n1 * n2 es de: ", n1 * n2)
 
 multiplicar(int(input("ingresa el primer numero: ")),int(input("ingresa el segundo numero: ")))
+
+
+def mensaje3():
+    print("estoy en la rama ema")
