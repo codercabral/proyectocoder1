ede6a228d2c815a6045de31805836fb1491b4803
Ema Cabral <micorreo2@gmail.com>
2024-06-27 20:27:56 -0300

    ingreso la funcion multiplicar y dividir

--- ejemplo1.py
+++ ejemplo1.py
@@ -7,3 +7,8 @@ def sumar(n1,n2):
     print("el resultado de la suma entre n1 + n2 es de: ", n1 + n2)
 
 sumar(int(input("ingresa el primer numero: ")),int(input("ingresa el segundo numero: ")))
+
+def multiplicar(n1,n2):
+    print("el resultado de la multiplicacion entre n1 * n2 es de: ", n1 * n2)
+
+multiplicar(int(input("ingresa el primer numero: ")),int(input("ingresa el segundo numero: ")))
